29555cc3b2e230778f18e569ec390dd7555f8598
Araqelyan00 <aramaraqelyan503@gmail.com>
2024-07-24 14:28:37 +0400

    IO learning

--- src/homework/lesson7/lesson7_1/Person.java
+++ src/homework/lesson7/lesson7_1/Person.java
@@ -1,9 +1,13 @@
 package homework.lesson7.lesson7_1;
 
+import java.io.Serial;
 import java.io.Serializable;
-import java.util.Set;
 
 public class Person implements Serializable {
+    @Serial
+    private static final long serialVersionUID = -631622909235584360L;
+    //The 'serialVersionUID = -631622909235584360L' is a unique ID of this class. If we change this class we need to regenerate this id.
+    //If we don't need to serialize any property we can type after word private a key word transient.
     private int id;
     private String name;
 
